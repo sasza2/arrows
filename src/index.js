bc792fa0f2d694b9cf5a5550ba7d88c7d8302043
sasza <sasza@onet.eu>
2019-10-08 21:34:32 +0200

    eslint jsx

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
-import Element from './jsx/element'
-import ends from './arrow/ends'
-import path from './arrow/path'
-import { DIRECTION } from './consts'
+import Element from './jsx/element';
+import ends from './arrow/ends';
+import path from './arrow/path';
+import { DIRECTION } from './consts';
 
 const arrow = ({ from, to }) => {
-  const arrow = path(ends(from), ends(to))
+  const arrow = path(ends(from), ends(to));
   const node = (
-    <svg style={{ top: arrow.offset.y, left: arrow.offset.x, fill: '#123456', position: 'absolute' }} width={arrow.size.x} height={arrow.size.y}>
+    <svg style={{
+      top: arrow.offset.y, left: arrow.offset.x, fill: '#123456', position: 'absolute',
+    }} width={arrow.size.x} height={arrow.size.y}>
       <path d={arrow.points} stroke="black" fill="transparent"/>
     </svg>
-  )
+  );
 
-  document.body.appendChild(node)
-}
+  document.body.appendChild(node);
+};
 
 window.addEventListener('load', () => {
   arrow({
@@ -24,7 +26,7 @@ window.addEventListener('load', () => {
     to: {
       direction: DIRECTION.TOP,
       node: document.getElementById('b'),
-      translation: [-0.4, 1]
+      translation: [-0.4, 1],
     },
-  })
-})
+  });
+});
