9cf81b9bd64322bc6475a831b3e3fd195ab77bca
sasza <sasza@onet.eu>
2020-04-23 20:15:17 +0200

    head image

---
+++ src/arrow/head/types/image.js
@@ -0,0 +1,23 @@
+import Element from 'jsx/element';
+
+const SVG_NS = 'http://www.w3.org/2000/svg';
+const XLINK_NS = 'http://www.w3.org/1999/xlink';
+
+const image = ({ src, width, height }) => {
+  if (!src || !width || !height) throw new Error('image requires src, height, width');
+
+  const node = document.createElementNS(SVG_NS, 'image');
+  node.setAttributeNS(null, 'width', width);
+  node.setAttributeNS(null, 'height', height);
+  node.setAttributeNS(null, 'x', -width / 2);
+  node.setAttributeNS(null, 'y', -height / 2);
+  node.setAttributeNS(XLINK_NS, 'xlink:href', src);
+
+  return {
+    node,
+    width,
+    height,
+  };
+};
+
+export default image;
