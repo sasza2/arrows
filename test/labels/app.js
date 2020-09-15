5307d1239515ec2b8533bbc64e8c369daa0e1559
sasza <sasza@onet.eu>
2020-09-15 23:33:20 +0200

    labels example

---
+++ test/labels/app.js
@@ -0,0 +1,30 @@
+window.addEventListener('load', function(){
+  const labels = document.querySelectorAll('.label')
+
+  const arrowProps = {
+    className: 'arrow',
+    from: {
+      node: document.getElementById('from'),
+      direction: 'top',
+      translation: [-0.5, -1],
+    },
+    to: {
+      node: document.getElementById('to'),
+      direction: 'top',
+      translation: [0.9, 1],
+    },
+    onChange: ({ getPointXY, ...x }) => {
+      labels.forEach(function(label) {
+        const position = getPointXY(parseFloat(label.getAttribute('data-distance')))
+        label.style.top = position.y + 'px'
+        label.style.left = position.x + 'px'
+        if (label.getAttribute('data-rotate')) {
+          label.style.transform = 'rotate(' + position.degree + 'deg)'
+        }
+      })
+    }
+  }
+
+  arrow = arrowCreate(arrowProps)
+  document.body.appendChild(arrow.node)
+})
