b9f3270c9942c114601229f0022aefd0faf73c93
sasza <sasza@onet.eu>
2021-01-05 00:21:33 +0100

    export order

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,6 @@ assignArrowCreate(window);
 
 export default arrowCreate;
 export { Anchor } from './anchor';
-export * from './interfaces/IArrow';
-export { HEAD, Head, HeadFactory } from './head';
 export { DIRECTION } from './consts';
+export { HEAD, Head, HeadFactory } from './head';
+export * from './interfaces/IArrow';
