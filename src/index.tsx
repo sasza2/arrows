7dcd2e5772264999aa2a3c3bb42ac08fea46a2d6
sasza <sasza@onet.eu>
2021-01-06 01:32:55 +0100

    remove exports

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,5 @@ import arrowCreate from './arrowCreate'
 assignArrowCreate(window);
 
 export default arrowCreate;
-export { Anchor } from './anchor';
 export { DIRECTION } from './consts';
-export { HEAD, Head, HeadFactory } from './head';
-export * from './interfaces/IArrow';
+export { HEAD } from './head';
