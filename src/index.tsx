541ba963aee85a6fd5b1b3a72718652e28b1e381
sasza <sasza@onet.eu>
2024-04-14 20:20:08 +0200

    feat: export helpers

--- src/index.tsx
+++ src/index.tsx
@@ -6,5 +6,8 @@ assignArrowCreate(window);
 export default arrowCreate;
 export { arrowCreate }
 export { DIRECTION } from './consts';
-export { HEAD } from './head';
+export { castToAnchor } from './anchor';
+export { default as arrowVector } from './arrowVector';
+export { headTransformCSS, HeadWithPoint, HEAD } from './head';
 export { IArrow, IArrowComponentProps, IArrowProps } from './interfaces/IArrow';
+export { default as observer } from './observer/observer';
